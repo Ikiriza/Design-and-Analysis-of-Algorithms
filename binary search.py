56bbb1ca10be4ac2a30e94ddcd40432187894335
Gensi Collin <99279784+Ikiriza@users.noreply.github.com>
2023-03-13 13:18:37 +0300

    Update binary search.py

--- binary search.py
+++ binary search.py
@@ -16,15 +16,16 @@ def binary_search(list, target):
 
     return None
 
+
 def verify(index):
     if index is not None:
         print("Target found at index: ", index)
     else:
         print("Target not found in list")
 
-numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 
-result = binary_search(numbers, 15)
+result = binary_search(numbers, 16)
 verify(result)
 
 result = binary_search(numbers, 7)
