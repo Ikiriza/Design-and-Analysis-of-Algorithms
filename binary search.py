05374a8363becd0e29a8db9a9f556bc0fe94c6f2
Gensi Collin <99279784+Ikiriza@users.noreply.github.com>
2023-05-02 08:23:06 +0300

    Update binary search.py

--- binary search.py
+++ binary search.py
@@ -3,6 +3,7 @@ def binary_search(list, target):
     first = 0
     last = len(list) - 1
 
+    
     while first <= last:
         midpoint = (first + last) // 2
 
@@ -12,7 +13,6 @@ def binary_search(list, target):
             first = midpoint + 1 
         else:            
       last = midpoint - 1
-
     return None
 
 def verify(index):
